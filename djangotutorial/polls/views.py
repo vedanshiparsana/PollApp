aebd917b17099038725eb4b4dd2f27cf1a756fdc
vedanshiparsana <vedanshiparsana@gmail.com>
2026-01-23 12:04:48 +0530

    Update views.py

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -9,3 +9,16 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+from django.http import HttpResponse
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
