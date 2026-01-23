e304cab3240311054bccd30e4188517eb16e2436
vedanshiparsana <vedanshiparsana@gmail.com>
2026-01-23 12:00:21 +0530

    Update views.py

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
 
 
-def index(request):
-    return HttpResponse("Helloooo, world. You're at the polls index.")
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
