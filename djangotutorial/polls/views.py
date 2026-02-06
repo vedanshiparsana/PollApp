1d26817090c056fc0dd1b53269f4b19f5ca0f7bc
Your Name <you@example.com>
2026-02-06 18:45:32 +0530

    Tutorial 4

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 
 def results(request, question_id):
@@ -9,16 +25,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-from django.http import HttpResponse
-from django.template import loader
-
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
-# Leave the rest of the views (detail, results, vote) unchanged
